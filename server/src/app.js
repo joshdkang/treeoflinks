bea7e4bf1545ced2921615bb40bb60c6f4ece81e
joshdkang <joshdkang@gmail.com>
2021-06-06 17:55:22 -0700

    Update cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'jdk-treeoflinks.netlify.app'
+      'https://jdk-treeoflinks.netlify.app'
     ],
     credentials: true
   })

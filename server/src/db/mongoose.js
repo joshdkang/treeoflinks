d2d7dce24d7e5dc19b811ded172365995b6390cd
joshdkang <joshdkang@gmail.com>
2021-06-08 22:30:23 -0700

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// /Users/Josh/mongodb/bin/mongod.exe --dbpath=/Users/Josh/mongodb-data
-// mongodb://127.0.0.1:27017/tree-of-links-api
-
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
